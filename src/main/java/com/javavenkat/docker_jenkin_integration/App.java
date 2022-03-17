2f4ef0d857ac399df251932b7ca6ca2758e89df9
venkat <radheshyam@gmail.com>
2022-03-17 09:46:50 +0530

    first commit

---
+++ src/main/java/com/javavenkat/docker_jenkin_integration/App.java
@@ -0,0 +1,13 @@
+package com.javavenkat.docker_jenkin_integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
